564ed8299a8118261276a9038acce4a9c4865fed
Marudhu Paandian.K <46159834+marudhu2004@users.noreply.github.com>
2023-07-08 12:00:46 +0530

    added logic to save in db

--- portal/models.py
+++ portal/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class Document(models.Model):
 
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
 
     def __str__(self) -> str:
         return f"{self.name}"
