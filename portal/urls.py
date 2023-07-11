a0c803b61fe5b269e548a43cf3d48d2363e58791
Marudhu Paandian.K <46159834+marudhu2004@users.noreply.github.com>
2023-07-11 11:03:51 +0530

    fixed wrong redirect

--- portal/urls.py
+++ portal/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.home, name='home'),
     path("edit/<admission_no>", views.edit, name="edit"),
     path("view/<admission_no>", views.view, name="view"),
-    path("pdf/<admission_no>", views.pdf_download, name="view")
+    path("pdf/<admission_no>", views.pdf_download, name="pdf")
 ]
