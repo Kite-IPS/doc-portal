508bc1acaaad644a909b035e09a5d7d8b87b918b
NishanthSbz <nishanthsbz05@gmail.com>
2023-07-19 11:52:10 +0530

    created a js that shows a pop up msg in edit page

---
+++ portal/static/js/popup.js
@@ -0,0 +1,6 @@
+// Function to show the popup div when the submit button is clicked
+// Function to show the popup div when the submit button is clicked
+function showPopup() {
+    var popup = document.getElementById("popupDiv");
+    popup.style.display = "block";
+}
