9cc4e741f2a1e97fb254f6f6c5d4c03243a063ff
Marudhu Paandian.K <46159834+marudhu2004@users.noreply.github.com>
2023-07-19 11:19:05 +0530

    added email settings

--- doc_portal/settings.py
+++ doc_portal/settings.py
@@ -125,3 +125,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_PORT = 465
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
